4ef62a14e384f3f088fde5d65b88ff0df00a1e08
Moto-Git <cubebz11h16@gmail.com>
2020-09-13 09:45:09 +0900

    first commit

---
+++ test/controllers/tasks_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class TasksControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get tasks_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get tasks_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get tasks_new_url
+    assert_response :success
+  end
+
+  test "should get edhit" do
+    get tasks_edhit_url
+    assert_response :success
+  end
+
+end
